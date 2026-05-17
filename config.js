31f3e2720dfeaa4e32af052d32b8b17c0e1bca65
mithiladhevendran-blip <mithila.dhevendran@gmail.com>
2026-05-17 15:51:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,13 @@ const S_URL =
 'https://funcvigewkhauenqxrbf.supabase.co';
 
 const S_KEY =
-'PASTE_YOUR_REAL_ANON_KEY_HERE';
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1bmN2aWdld2toYXVlbnF4cmJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1NTM1MzQsImV4cCI6MjA5NDEyOTUzNH0.INR7nkrv-CV80i97SyaHwdZd3chlQjaVgNsQY39Nqxo';
 
 const _supabase =
-    supabase.createClient(S_URL, S_KEY);
+    supabase.createClient(
+        S_URL,
+        S_KEY
+    );
 
 let isRedirecting = false;
 
